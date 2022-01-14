f7750aec08e2b40f3fff6b45211ebe6aabc80e27
Cesar Augusto Pesantez Carrion <craupesantez@gmail.com>
2022-01-13 22:59:45 -0500

    habilitar cities y translate

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -3,7 +3,7 @@
         <ul class="nav">
             <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.content') }}</li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/roles') }}"><i class="nav-icon icon-diamond"></i> {{ trans('admin.role.title') }}</a></li>
-           {{--  <li class="nav-item"><a class="nav-link" href="{{ url('admin/cities') }}"><i class="nav-icon icon-ghost"></i> {{ trans('admin.city.title') }}</a></li>  --}}
+           <li class="nav-item"><a class="nav-link" href="{{ url('admin/cities') }}"><i class="nav-icon icon-ghost"></i> {{ trans('admin.city.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/specialties') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.specialty.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/exams') }}"><i class="nav-icon icon-globe"></i> {{ trans('admin.exam.title') }}</a></li>
            <li class="nav-item"><a class="nav-link" href="{{ url('admin/people') }}"><i class="nav-icon icon-umbrella"></i> {{ trans('admin.person.title') }}</a></li>
@@ -16,7 +16,7 @@
 
             <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.settings') }}</li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/admin-users') }}"><i class="nav-icon icon-user"></i> {{ __('Manage access') }}</a></li>
-            {{--  <li class="nav-item"><a class="nav-link" href="{{ url('admin/translations') }}"><i class="nav-icon icon-location-pin"></i> {{ __('Translations') }}</a></li>  --}}
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/translations') }}"><i class="nav-icon icon-location-pin"></i> {{ __('Translations') }}</a></li>
             
             {{-- Do not delete me :) I'm also used for auto-generation menu items --}}
             {{--  <li class="nav-item"><a class="nav-link" href="{{ url('admin/configuration') }}"><i class="nav-icon icon-settings"></i> {{ __('Configuration') }}</a></li>  --}}
